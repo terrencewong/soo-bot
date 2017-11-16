8985aa87795a536af78a986816244a6e81bad985
Terrence Wong <wong.terrence@hotmail.com>
2017-11-16 15:27:48 -0800

    revert

--- bot.js
+++ bot.js
@@ -9,7 +9,7 @@ function randomInt(max) {
 
 var options = [];
 
-client.on('msg', (msg) => {
+client.on('message', (msg) => {
   if ((msg.content === 'soo') || (msg.content === '!roll')) {
     if(options.length != 0) {
       msg.channel.send(options[randomInt(optionsList.length-1)]);
