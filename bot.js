dc4cf204ca7a88de046a07ea9ed265a080e60599
terrence wong <wong.terrence@hotmail.com>
2018-01-15 00:13:55 -0800

    change array name to not start with a num

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function randomInt(max) {
 
 var options = [];
 
-var 8ballOptions= ["It is certain"];
+var eightBallOptions= ['It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Do not count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful'];
 
 function check(temp){
   if(options.indexOf(temp) == -1){
@@ -59,7 +59,7 @@ client.on('message', (msg) => {
   }
    
   if(msg.content === '!8'){
-    msg.channel.send(8ballOptions[randomInt(20)]);
+    msg.channel.send(eightBallOptions[randomInt(eightBallOptions.length-1)]);
   }
 });
 
