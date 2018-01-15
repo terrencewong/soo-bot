54562987c11ccacc3f4774fe8bd62910108587bd
terrence wong <wong.terrence@hotmail.com>
2018-01-15 00:18:39 -0800

    add more negative responses to balance out affirmative

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function randomInt(max) {
 
 var options = [];
 
-var eightBallOptions= ['It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Do not count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful'];
+var eightBallOptions= ['It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Do not count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful','No','Hell no','Fuck no','No, of course not','No and stop asking'];
 
 function check(temp){
   if(options.indexOf(temp) == -1){
