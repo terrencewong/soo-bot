a3fae264645d4e5cdeaf8e5fbf540ee6e2770ebf
terrence wong <wong.terrence@hotmail.com>
2018-01-15 00:11:11 -0800

    testing string array

--- bot.js
+++ bot.js
@@ -8,7 +8,7 @@ function randomInt(max) {
 
 var options = [];
 
-var 8ballOptions= ['It is certain','It is decidedly so','Without a doubt','Yes definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Do not count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful'];
+var 8ballOptions= ["It is certain"];
 
 function check(temp){
   if(options.indexOf(temp) == -1){
